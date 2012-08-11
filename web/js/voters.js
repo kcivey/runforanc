f93515f4b3719079cd371b34ce02a587203af955
Keith C. Ivey <keith@iveys.org>
2012-08-11 16:31:02 -0400

    use timeout to handle JSONP errors

--- web/js/voters.js
+++ web/js/voters.js
@@ -2,7 +2,11 @@ jQuery(function ($) {
 
     $('#form-search').submit(function (evt) {
         var searchData = {},
-            button = $('button', this);
+            button = $('button', this),
+            resetButton = function () {
+                button.text('Search').removeAttr('disabled');
+            },
+            timeoutHandle;
         evt.preventDefault();
         $.each(['q', 'name', 'address'], function (i, name) {
             var value = $('#' + name).val();
@@ -15,12 +19,24 @@ jQuery(function ($) {
         }
         button.text('Please Wait').attr('disabled', 'disabled');
         $('#results, #explanation').empty();
+        // Use a timeout because JSONP calls don't always raise error
+        // events when there's a problem.
+        timeoutHandle = setTimeout(
+            function () {
+                alert('Something unexpected went wrong with the search request. Trying again might work.');
+                resetButton();
+            },
+            10000
+        );
         $.ajax({
             url: 'http://' + window.location.host + ':3000/search',
             data: searchData,
             dataType: 'jsonp',
             success: handleResults,
-            complete: function () { button.text('Search').removeAttr('disabled'); }
+            complete: function () {
+                clearTimeout(timeoutHandle);
+                resetButton();
+            }
         });
     });
 
